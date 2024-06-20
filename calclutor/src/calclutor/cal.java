cfbfe66bc8b2713b2ff619ca6993a075e8df45ce
DELL <DELL@DESKTOP-RQ43MF3>
2024-06-20 09:58:31 +0530

    Initial Commit

---
+++ calclutor/src/calclutor/cal.java
@@ -0,0 +1,26 @@
+package calclutor;
+import javax.swing.*;
+public class cal {
+
+	static void 	add(int a, int b) {
+		System.out.println(a+b);
+	} 
+	
+	static int 	add(int a, int b,int c) {
+		return a+c+b;
+	}
+	static void 	div(float a, float b) {
+		System.out.println(a/b);
+	}
+	static void 	div(int a, int b) {
+		System.out.println(a/b);
+	}
+	static {
+		add(10,3);
+	}
+	
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog(add(1,10,1));
+	}
+
+}
